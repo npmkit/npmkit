b0530eca642dc8d5ea947c4ff746da374d813d33
Sergey Bekrin <sergey@bekrin.me>
2018-07-15 23:11:36 +0200

    fix: use some serious icons instead emojis

--- app/src/tray-popup/components/toolbar/action.js
+++ app/src/tray-popup/components/toolbar/action.js
@@ -2,7 +2,13 @@ import styled from 'styled-components';
 import Button from '~/common/components/button';
 
 const ToolbarAction = styled(Button).attrs({ ghost: true })`
+  transition: color 200ms ease;
   font-size: 1.25rem;
+  color: #999;
+
+  &:hover {
+    color: #333;
+  }
 `;
 
 export default ToolbarAction;
